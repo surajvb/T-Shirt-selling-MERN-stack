a8f84c34c40d83ea481aa3ff305418cdf52ee000
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2020-05-24 00:21:57 +0530

    adding virtual field like password and adding one more method like authenticate.

--- lcomernbootcamp/projbackend/models/user.js
+++ lcomernbootcamp/projbackend/models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
 
 var userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +24,6 @@ var userSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  //TODO: come back here
   encry_password: {
     type: String,
     required: true
@@ -38,7 +39,22 @@ var userSchema = new mongoose.Schema({
   }
 });
 
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
 userSchema.method = {
+  autheticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
   securePassword: function(plainpassword) {
     if (!password) return "";
     try {
