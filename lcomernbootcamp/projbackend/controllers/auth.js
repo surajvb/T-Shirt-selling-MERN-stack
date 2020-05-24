205986baeab4b974aa3f876fcb3d7a2cb74a00b7
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2020-05-24 12:43:35 +0530

    Adding simple signup controller

--- lcomernbootcamp/projbackend/controllers/auth.js
+++ lcomernbootcamp/projbackend/controllers/auth.js
@@ -1,3 +1,8 @@
+exports.signup = (req, res) => {
+  res.json({
+    message: "User signup successfully"
+  });
+};
 exports.signout = (req, res) => {
   res.json({
     message: "User signout inside controller"
