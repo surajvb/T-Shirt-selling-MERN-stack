df4dd8c71d4de8fec396206e37871f3716c06b2e
Suraj Veerappa Bolanavar <SBolanavar@synamedia.com>
2020-05-24 13:14:57 +0530

    Saving the user into database

--- lcomernbootcamp/projbackend/controllers/auth.js
+++ lcomernbootcamp/projbackend/controllers/auth.js
@@ -1,11 +1,23 @@
+const User = require("../models/user");
+
 exports.signup = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "User signup successfully"
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB"
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
   });
 };
+
 exports.signout = (req, res) => {
   res.json({
-    message: "User signout inside controller"
+    message: "User signout"
   });
 };
